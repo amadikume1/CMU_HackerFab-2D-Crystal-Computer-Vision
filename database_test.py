4303cec794e5c8b1783d755938d0a8e1a06ad3be
amadikume1 <you@example.com>
2025-09-12 10:12:32 -0400

    database_csv commit

--- database_test.py
+++ database_test.py
@@ -1,5 +1,5 @@
 import sqlite3 as sq
-
+import csv
 database = sq.connect("Database/substrate_database")
 
 cursor = database.cursor()
@@ -9,7 +9,7 @@ cursor = database.cursor()
 # Will interpret data as basic list.
 
 information = [["Wafer1", "Graphene", "Triangle", 10, 10, "GREEN", 150, 180],
-               ["Wafer2", "Graphene", "Square", 15, 14, "GREEN", 100, 12]]
+               ["Wafer2", "Graphene", "Square", 15, 10, "GREEN", 150, 180]]
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS substrate (
                
@@ -34,6 +34,20 @@ for entry in information:
 
 cursor.execute("SELECT * FROM substrate")
 rows = cursor.fetchall()
+
+header = []
+
+for head in cursor.description:
+    print(head)
+    header.append(head[0])
+
+with open('substate.csv', 'w', newline='') as f:
+
+    write = csv.writer(f)
+
+    write.writerow(header)
+
+    write.writerows(rows)
 
 # Print each row
 for row in rows:
