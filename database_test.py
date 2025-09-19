5e9d437db63ad3151ff24236116f4a4e0eaa9d0f
amadikume1 <96507679+amadikume1@users.noreply.github.com>
2025-09-19 15:32:08 -0400

    Update database_test.py

--- database_test.py
+++ database_test.py
@@ -1,33 +1,64 @@
 import sqlite3 as sq
 import csv
+from test import *
+
 database = sq.connect("Database/substrate_database")
 
 cursor = database.cursor()
 
+
+
 # Will interpret data as basic list.
 
-information = [["Wafer1", "Graphene", "Triangle", 10, 10, "GREEN", 150, 180],
-               ["Wafer2", "Graphene", "Square", 15, 10, "GREEN", 150, 180]]
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS substrate (
-               
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
-               Wafer_ID TEXT,
-               Material TEXT,
-               Shape TEXT,
-               Size_Width REAL,
-               Size_Height REAL,
-               Color TEXT,
-               Position_X REAL,
-               Position_Y REAL
-
-               )'''
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS substrate (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        idx INTEGER,
+        area REAL,
+        perimeter REAL,
+        aspect_ratio REAL,
+        solidity REAL,
+        circularity REAL,
+        centroid_x REAL,
+        centroid_y REAL,
+        bbox_x INTEGER,
+        bbox_y INTEGER,
+        bbox_w INTEGER,
+        bbox_h INTEGER,
+        entropy REAL
     )
+''')
 
 
 
-for entry in information:
-    cursor.execute(f"INSERT INTO substrate (Wafer_ID, Material, Shape, Size_Width, Size_Height, Color, Position_X, Position_Y) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (entry[0],entry[1], entry[2], entry[3], entry[4],entry[5], entry[6], entry[7],))
+for entry in shape_features:
+
+  
+    cursor.execute(
+        '''
+        INSERT INTO substrate (
+            idx, area, perimeter, aspect_ratio, solidity, circularity,
+            centroid_x, centroid_y, bbox_x, bbox_y, bbox_w, bbox_h, entropy
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (
+            entry["idx"],
+            entry["area"],
+            entry["perimeter"],
+            entry["aspect_ratio"],
+            entry["solidity"],
+            entry["circularity"],
+            entry["centroid_x"],
+            entry["centroid_y"],
+            entry["bbox_x"],
+            entry["bbox_y"],
+            entry["bbox_w"],
+            entry["bbox_h"],
+            entry["entropy"],
+        )
+    )
+    
 
 
 cursor.execute("SELECT * FROM substrate")
@@ -36,10 +67,10 @@ rows = cursor.fetchall()
 header = []
 
 for head in cursor.description:
-    # print(head)
+    print(head)
     header.append(head[0])
 
-with open('Database/substrate.csv', 'w', newline='') as f:
+with open('substate.csv', 'w', newline='') as f:
 
     write = csv.writer(f)
 
@@ -48,5 +79,6 @@ with open('Database/substrate.csv', 'w', newline='') as f:
     write.writerows(rows)
 
 # Print each row
-for row in rows:
-    print(row)
+# for row in rows:
+#     print(row)
+# 
